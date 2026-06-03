f843c5b662c67539d4469c102bcd7895f08b0f49
taku26841397 <taku26841397@gmail.com>
2026-06-03 21:53:38 +0900

    add urls.py

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('list/',views.TodoListView.as_view(),name='todo_list'),
     path('<int:pk>/',views.TodoDetailView.as_view(),name='todo_detail'),
     path('new/',views.TodoCreateView.as_view(),name='todo_create'),
-    path('<int:pk>/edit',views.TodoUpdateView.as_view(),name='todo_update'),
-    path('<int:pk>/delete',views.TodoDeleteView.as_view(),name='todo_delete'),
+    path('<int:pk>/edit/',views.TodoUpdateView.as_view(),name='todo_update'),
+    path('<int:pk>/delete/',views.TodoDeleteView.as_view(),name='todo_delete'),
     path('analytics/',views.TodoAnalyticsView.as_view(),name='todo_analytics'),
     
 ]
